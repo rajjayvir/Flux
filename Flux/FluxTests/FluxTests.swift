4c81a6ec5af024629a321973cde2a757db2be072
Jayvirsinh <jy712996@dal.ca>
2024-11-16 23:10:52 -0400

    Added the project structure

---
+++ Flux/FluxTests/FluxTests.swift
@@ -0,0 +1,17 @@
+//
+//  FluxTests.swift
+//  FluxTests
+//
+//  Created by Jayvirsinh Raj on 2024-11-16.
+//
+
+import Testing
+@testable import Flux
+
+struct FluxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
