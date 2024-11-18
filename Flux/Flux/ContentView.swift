272f40298e119c6701f77edd4f62469ea951a2e0
Jayvirsinh <jy712996@dal.ca>
2024-11-18 16:48:35 -0400

    entry point and splash screen added

--- Flux/Flux/ContentView.swift
+++ Flux/Flux/ContentView.swift
@@ -2,5 +2,47 @@
 //  ContentView.swift
 //  Flux
 //
-//  Created by Jayvirsinh Raj on 2024-11-16.
+//  Created by Jayvirsinh Raj on 2024-11-18.
 //
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isActive = false
+
+    var body: some View {
+        ZStack {
+            Color("backgroundColor")
+                .ignoresSafeArea()
+            if isActive {
+                // Navigate to the main view after the splash screen
+//                HomeView() // Replace this with your main home view
+            } else {
+                // Splash screen content
+                VStack {
+                    Image("fluxIcon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                    Text("Flux")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        withAnimation {
+                            isActive = true
+                        }
+                    }
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flux/Flux/SwiftUIView.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftUIView.swift
+//  Flux
+//
+//  Created by Jayvirsinh Raj on 2024-11-18.
+//
+
+import SwiftUI
+
+@main
+struct FluxApp: App {
+    // State object to manage app-wide data
+//    @StateObject private var healthViewModel = HealthViewModel()
+//    @StateObject private var workoutViewModel = WorkoutViewModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+//                .environmentObject(healthViewModel)  // Providing the ViewModels to the environment
+//                .environmentObject(workoutViewModel)
+        }
+    }
+}
+
